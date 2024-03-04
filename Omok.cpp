fd332f50a030b2399732300dccdfe95ac14540c8
HankiPark <89247586+HankiPark@users.noreply.github.com>
2024-03-04 20:37:30 +0900

    명칭 수정, 우승시 이펙트 추가, 컴퓨터 패턴 개선

--- Omok.cpp
+++ Omok.cpp
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-#define X_LINE		19
-#define Y_LINE		19
+#define WIDTH_LINE		19
+#define HEIGHT_LINE		19
 
 #define WHITE_SPACE 50
 
@@ -21,110 +21,166 @@ using namespace std;
 
 #define POS(x)		(WHITE_SPACE + (x) * INTERVAL)
 
-unsigned int dol[Y_LINE][X_LINE];
-unsigned int order = 1;
+#define FOUR_SCORE	7
+#define WIN_SCORE	100
+
+#define PII			pair<int, int>
+
+
+#define BLACK		1
+#define WHITE		2
+#define BLACK_WIN	3
+#define WHITE_WIN	4
+
+unsigned int dol[HEIGHT_LINE][WIDTH_LINE];
+unsigned int order = BLACK;
 unsigned int game_mode = 0;
-pair<int, int> computer_dol;
+vector<PII> completed_dol_loc;
+//pair<int, int> computer_dol;
 
 void drawMap(HWND hand);
-void checkNear(int y, int x, vector<vector<int>>& visited);
-pair<int, int> whiteAI();
-pair<int, int> buttonDown(HWND hwnd, int x, int y);
-int checkLineValue(pair<int, int> loc);
+void checkNear(int height, int width, vector<vector<int>>& visited);
+PII whiteAI();
+PII buttonDown(HWND hwnd, int height, int width);
+int checkLineValue(PII loc);
 LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
 int WINAPI main(HINSTANCE hIns, HINSTANCE hPrevIns, LPSTR lpCmdLine, int nCmdShow);
 
+//좌표에 대해 (x, y)는 x는 세로, y는 가로로 지정 dol[x][y];
 
 void drawMap(HWND hwnd) {
 	PAINTSTRUCT ps;
 	HDC hdc = BeginPaint(hwnd, &ps);
+	int check_line = 0;
 
-	for (int i = 0; i < X_LINE; i++) {
+	for (int i = 0; i < WIDTH_LINE; i++) {
 		MoveToEx(hdc, POS(i), POS(0), NULL);
-		LineTo(hdc, POS(i), POS(Y_LINE - 1));
+		LineTo(hdc, POS(i), POS(HEIGHT_LINE - 1));
 	}
 
-	for (int i = 0; i < Y_LINE; i++) {
+	for (int i = 0; i < HEIGHT_LINE; i++) {
 		MoveToEx(hdc, POS(0), POS(i), NULL);
-		LineTo(hdc, POS(X_LINE - 1), POS(i));
+		LineTo(hdc, POS(WIDTH_LINE - 1), POS(i));
 	}
 
-	for (int i = 0; i < Y_LINE; i++) {
-		for (int j = 0; j < X_LINE; j++) {
+	for (int i = 0; i < HEIGHT_LINE; i++) {
+		for (int j = 0; j < WIDTH_LINE; j++) {
 			if (dol[i][j] > 0) {
-				if (dol[i][j] == 1) {
+				if (dol[i][j] == BLACK) {
 					SelectObject(hdc, GetStockObject(BLACK_BRUSH));
+					Ellipse(hdc, POS(j) - RADIUS, POS(i) - RADIUS, POS(j) + RADIUS, POS(i) + RADIUS);
 				}
-				else {
+				else if (dol[i][j] == WHITE) {
 					SelectObject(hdc, GetStockObject(WHITE_BRUSH));
+					Ellipse(hdc, POS(j) - RADIUS, POS(i) - RADIUS, POS(j) + RADIUS, POS(i) + RADIUS);
 				}
-				Ellipse(hdc, POS(j) - RADIUS, POS(i) - RADIUS, POS(j) + RADIUS, POS(i) + RADIUS);
+				else if (dol[i][j] == BLACK_WIN) {
+					check_line = 1;
+					SelectObject(hdc, GetStockObject(BLACK_BRUSH));
+					Ellipse(hdc, POS(j) - RADIUS * 1.2, POS(i) - RADIUS * 1.2, POS(j) + RADIUS * 1.2, POS(i) + RADIUS * 1.2);
+				}
+				else if (dol[i][j] == WHITE_WIN) {
+					check_line = 1;
+					SelectObject(hdc, GetStockObject(WHITE_BRUSH));
+					Ellipse(hdc, POS(j) - RADIUS * 1.2, POS(i) - RADIUS * 1.2, POS(j) + RADIUS * 1.2, POS(i) + RADIUS * 1.2);
+				}
+				
 			}
 		}
+	}
+
+
+	if (check_line == 1) {
+		HPEN hPen = CreatePen(PS_SOLID, 5, RGB(237, 28, 36));
+		HPEN hOldPen = (HPEN)SelectObject(hdc, hPen);
+
+		MoveToEx(hdc, POS(completed_dol_loc[0].second), POS(completed_dol_loc[0].first),  NULL);
+		LineTo(hdc, POS(completed_dol_loc.back().second),  POS(completed_dol_loc.back().first));
+
+		SelectObject(hdc, hOldPen);
+		DeleteObject(hPen);
+		
 	}
 
 	EndPaint(hwnd, &ps);
 }
 
-void checkNear(int y, int x, vector<vector<int>>& visited) {
-	int dx[8] = {1, 1, 1, 0, 0, -1, -1, -1};
-	int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+void checkNear(int height, int width, vector<vector<int>>& visited) {
+	int dh[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+	int dw[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 	int max_value = 0;
 	for (int i = 0; i < 8; i++) {
-		int newx = x + dx[i];
-		int newy = y + dy[i];
-		if (newx < 0 || newx >= X_LINE || newy < 0 || newy >= Y_LINE) {
+		int new_height = height + dh[i];
+		int new_width = width + dw[i];
+		if (new_height < 0 || new_width >= WIDTH_LINE || new_width < 0 || new_height >= HEIGHT_LINE) {
 			continue;
 		}
-		if (dol[newy][newx] != 0) {
-			continue;
+		// 돌이 들어갔을 경우 값 계산을 위함
+		if (dol[new_height][new_width] == 0) {
+			dol[new_height][new_width] = order;
+			int t = checkLineValue(PII(new_height, new_width));
+			dol[new_height][new_width] = 0;
+			// 자기돌에 가중치 부여
+			if (order == WHITE) {
+				t = t * 1.5;
+			}
+			visited[new_height][new_width] = max(visited[new_height][new_width], t);
 		}
-		dol[newy][newx] = order;
-		int t = checkLineValue(pair<int, int>(newx, newy));
-		dol[newy][newx] = 0;
-		visited[newy][newx] = max(visited[newy][newx], t);
+
 	}
 	
 
 }
 
-//현재 놓여있는 돌들의 점수를 계산해서 위치 파악하기?
-pair<int, int> whiteAI() {
-	vector<vector<int>> visited(X_LINE, vector<int> (Y_LINE, 0));
-	for (int i = 0; i < Y_LINE; i++) {
-		for (int j = 0; j < X_LINE; j++) {
+//현재 놓여있는 돌들의 점수를 계산해서 위치 파악하기
+PII whiteAI() {
+	vector<vector<int>> visited(HEIGHT_LINE, vector<int> (WIDTH_LINE, 0));
+	for (int i = 0; i < HEIGHT_LINE; i++) {
+		for (int j = 0; j < WIDTH_LINE; j++) {
 			if (dol[i][j] == 0) {
 				continue;
 			}
 			checkNear(i, j, visited);
-			if (order == 1) {
-				order = 2;
+			if (order == BLACK) {
+				order = WHITE;
 			}
 			else {
-				order = 1;
+				order = BLACK;
 			}
 			checkNear(i, j, visited);
-			if (order == 1) {
-				order = 2;
+			if (order == BLACK) {
+				order = WHITE;
 			}
 			else {
-				order = 1;
+				order = BLACK;
 			}
 		}
 	}
 	int max_value = 0;
-	vector<pair<int, int>> marking;
+	vector<PII> marking;
 
-	for (int i = 0; i < Y_LINE; i++) {
-		for (int j = 0; j < X_LINE; j++) {
-			cout << visited[i][j] << " ";
+	for (int i = 0; i < HEIGHT_LINE; i++) {
+		for (int j = 0; j < WIDTH_LINE; j++) {
+			// 점수 체크용 cout
+			/*
+			if (visited[i][j] > 9) {
+				cout << visited[i][j] << "  ";
+			}
+			else if (visited[i][j] > 99) {
+				cout << visited[i][j] << " ";
+			}
+			else {
+				cout << visited[i][j] << "   ";
+			}
+			*/
+			
 			if (visited[i][j] > max_value) {
 				marking.clear();
-				marking.push_back(pair<int, int>(i, j));
+				marking.push_back(PII(i, j));
 				max_value = visited[i][j];
 			}
 			else if (visited[i][j] == max_value) {
-				marking.push_back(pair<int, int>(i, j));
+				marking.push_back(PII(i, j));
 			}
 		}
 		cout << endl;
@@ -136,174 +192,263 @@ pair<int, int> whiteAI() {
 	return { marking[choose].first, marking[choose].second };
 }
 
-pair<int, int> buttonDown(HWND hwnd, int x, int y) {
-	if (x > POS(0) - RADIUS && y > POS(0) - RADIUS &&
-		x < POS(X_LINE - 1) + RADIUS && y < POS(Y_LINE - 1) + RADIUS) {
-		int dol_x = (x - WHITE_SPACE + RADIUS) / INTERVAL;
-		int dol_y = (y - WHITE_SPACE + RADIUS) / INTERVAL;
+PII buttonDown(HWND hwnd, int height, int width) {
+	if (width > POS(0) - RADIUS && height > POS(0) - RADIUS &&
+		width < POS(WIDTH_LINE - 1) + RADIUS && height < POS(HEIGHT_LINE - 1) + RADIUS) {
+		int dol_width = (width - WHITE_SPACE + RADIUS) / INTERVAL;
+		int dol_height = (height - WHITE_SPACE + RADIUS) / INTERVAL;
+		// 1 vs 1 
 		if (game_mode == 1) {
-			if (dol[dol_y][dol_x] == 0) {
-				dol[dol_y][dol_x] = order;
-				if (order == 1) {
-					order++;
+			if (dol[dol_height][dol_width] == 0) {
+				dol[dol_height][dol_width] = order;
+				if (order == BLACK) {
+					order = WHITE;
 				}
 				else {
-					order--;
+					order = BLACK;
 				}
 				InvalidateRect(hwnd, NULL, TRUE);
 
-				return pair<int, int>(dol_x, dol_y);
+				return PII(dol_height, dol_width);
 			}
 		}
+		// vs computer
 		else {
-			if (dol[dol_y][dol_x] == 0) {
-				dol[dol_y][dol_x] = order;
-
+			if (dol[dol_height][dol_width] == 0) {
+				dol[dol_height][dol_width] = order;
 				InvalidateRect(hwnd, NULL, TRUE);
-				return pair<int, int>(dol_x, dol_y);
+				return PII (dol_height, dol_width);
 			}
 		}
 
 	}
 
-	return pair<int, int>(-1, -1);
+	return PII(-1, -1);
 }
 
 // 게임오버를 검사하는 함수에서, 컴퓨터가 알을 놓기위한 점수 계산방식으로 바꿈
-int checkLineValue(pair<int, int> loc) {
-	if (loc.first == -1 && loc.second == -1) {
-		return 0;
-	}
-	int x = loc.first;
-	int y = loc.second;
-	int dol_color = dol[y][x];
-	int garo = 1;
-	int sero = 1;
-	int tentofive = 1;
-	int seventotwo = 1;
+int checkLineValue(PII loc) {
+	int height = loc.first;
+	int width = loc.second;
+	int dol_color = dol[height][width];
+	int check_width = 1;
+	int check_height = 1;
+	int check_downward_diagonal = 1;
+	int check_upward_diagonal = 1;
 	int count = 0;
 	int max_value = 0;
+	vector<PII> temp;
 
-	for (int i = x - 1; i >= 0; i--) {
+	temp.push_back(loc);
+	for (int i = width - 1; i >= 0; i--) {
 		if (count++ > 5) {
 			break;
 		}
-		if (dol[y][i] == dol_color) {
-			garo++;
+		if (dol[height][i] == dol_color) {
+			check_width++;
+			temp.push_back({ height, i });
 		}
 		else {
 			break;
 		}
 	}
-
-	for (int i = x + 1; i <= X_LINE; i++) {
+	
+	for (int i = width + 1; i <= WIDTH_LINE; i++) {
 		if (count++ > 5) {
 			break;
 		}
-		if (dol[y][i] == dol_color) {
-			garo++;
+		if (dol[height][i] == dol_color) {
+			check_width++;
+			temp.push_back({ height, i });
 		}
 		else {
 			break;
 		}
 	}
-
-	max_value = max(max_value, garo);
+	
+	switch (check_width) {
+	case 0:
+		break;
+	case 1:
+		max_value += 1;
+		break;
+	case 2:
+		max_value += 3;
+		break;
+	case 3:
+		max_value += 5;
+		break;
+	case 4:
+		max_value += 20;
+		break;
+	default:
+		max_value += 100;
+		completed_dol_loc.clear();
+		completed_dol_loc.insert(completed_dol_loc.begin(), temp.begin(), temp.end());
+		break;
+	}
 
 	count = 0;
-
-	for (int i = y - 1; i >= 0; i--) {
+	temp.clear();
+	temp.push_back(loc);
+	for (int i = height - 1; i >= 0; i--) {
 		if (count++ > 5) {
 			break;
 		}
-		if (dol[i][x] == dol_color) {
-			sero++;
+		if (dol[i][width] == dol_color) {
+			check_height++;
+			temp.push_back({ i, width });
 		}
 		else {
 			break;
 		}
 	}
-
-	for (int i = y + 1; i <= Y_LINE; i++) {
+	
+	for (int i = height + 1; i <= HEIGHT_LINE; i++) {
 		if (count++ > 5) {
 			break;
 		}
-		if (dol[i][x] == dol_color) {
-			sero++;
+		if (dol[i][width] == dol_color) {
+			check_height++;
+			temp.push_back({ i, width });
 		}
 		else {
 			break;
 		}
 	}
-
-	max_value = max(max_value, sero);
+	
+	switch (check_height) {
+	case 0:
+		break;
+	case 1:
+		max_value += 1;
+		break;
+	case 2:
+		max_value += 3;
+		break;
+	case 3:
+		max_value += 5;
+		break;
+	case 4:
+		max_value += 20;
+		break;
+	default:
+		max_value += 100;
+		completed_dol_loc.clear();
+		completed_dol_loc.insert(completed_dol_loc.begin(), temp.begin(), temp.end());
+		break;
+	}
 
 	count = 0;
+	temp.clear();
+	temp.push_back(loc);
 
-	int temp_x = x - 1;
+	int temp_width = width - 1;
 
-	for (int i = y - 1; i >= 0; i--) {
-		if (temp_x < 0 || count++ > 5) {
+	for (int i = height - 1; i >= 0; i--) {
+		if (temp_width < 0 || count++ > 5) {
 			break;
 		}
-		if (dol[i][temp_x--] == dol_color) {
-			tentofive++;
+		if (dol[i][temp_width--] == dol_color) {
+			check_downward_diagonal++;
+			temp.push_back({ i, temp_width + 1 });
 		}
 		else {
 			break;
 		}
 	}
+	
+	temp_width = width + 1;
 
-	temp_x = x + 1;
-
-	for (int i = y + 1; i <= Y_LINE; i++) {
-		if (temp_x > X_LINE || count++ > 5) {
+	for (int i = height + 1; i <= HEIGHT_LINE; i++) {
+		if (temp_width > WIDTH_LINE || count++ > 5) {
 			break;
 		}
-		if (dol[i][temp_x++] == dol_color) {
-			tentofive++;
+		if (dol[i][temp_width++] == dol_color) {
+			check_downward_diagonal++;
+			temp.push_back({ i, temp_width - 1 });
 		}
 		else {
 			break;
 		}
 	}
-
-	max_value = max(max_value, tentofive);
+	
+	switch (check_downward_diagonal) {
+	case 0:
+		break;
+	case 1:
+		max_value += 1;
+		break;
+	case 2:
+		max_value += 3;
+		break;
+	case 3:
+		max_value += 5;
+		break;
+	case 4:
+		max_value += 20;
+		break;
+	default:
+		max_value += 100;
+		completed_dol_loc.clear();
+		completed_dol_loc.insert(completed_dol_loc.begin(), temp.begin(), temp.end());
+		break;
+	}
 
 	count = 0;
+	temp.clear();
+	temp.push_back(loc);
 
-	temp_x = x + 1;
-	for (int i = y - 1; i >= 0; i--) {
-		if (temp_x > X_LINE || count++ > 5) {
+	temp_width = width + 1;
+	for (int i = height - 1; i >= 0; i--) {
+		if (temp_width > WIDTH_LINE || count++ > 5) {
 			break;
 		}
-		if (dol[i][temp_x++] == dol_color) {
-			seventotwo++;
+		if (dol[i][temp_width++] == dol_color) {
+			check_upward_diagonal++;
+			temp.push_back({ i, temp_width - 1 });
 		}
 		else {
 			break;
 		}
 	}
-	temp_x = x - 1;
-	for (int i = y + 1; i <= Y_LINE; i++) {
-		if (temp_x < 0 || count++ > 5) {
+	
+	temp_width = width - 1;
+	for (int i = height + 1; i <= HEIGHT_LINE; i++) {
+		if (temp_width < 0 || count++ > 5) {
 			break;
 		}
-		if (dol[i][temp_x--] == dol_color) {
-			seventotwo++;
+		if (dol[i][temp_width--] == dol_color) {
+			check_upward_diagonal++;
+			temp.push_back({ i, temp_width + 1 });
 		}
 		else {
 			break;
 		}
 	}
+	
+	switch (check_upward_diagonal) {
+	case 0:
+		break;
+	case 1:
+		max_value += 1;
+		break;
+	case 2:
+		max_value += 3;
+		break;
+	case 3:
+		max_value += 5;
+		break;
+	case 4:
+		max_value += 20;
+		break;
+	default:
+		max_value += 100;
+		completed_dol_loc.clear();
+		completed_dol_loc.insert(completed_dol_loc.begin(), temp.begin(), temp.end());
+		break;
+	}
 
-	max_value = max(max_value, seventotwo);
-	if (max_value == 4) {
-		max_value = 7;
-	}
-	else if (max_value == 5) {
-		max_value = 10;
-	}
 	count = 0;
 
 	return max_value;
@@ -317,10 +462,10 @@ int checkGameOver(pair<int, int> loc) {
 	int x = loc.first;
 	int y = loc.second;
 	int dol_color = dol[y][x];
-	int garo = 1;
-	int sero = 1;
-	int tentofive = 1;
-	int seventotwo = 1;
+	int check_width = 1;
+	int check_height = 1;
+	int check_downward_diagonal = 1;
+	int check_upward_diagonal = 1;
 	int count = 0;
 
 	for (int i = x - 1; i >= 0; i--) {
@@ -328,26 +473,26 @@ int checkGameOver(pair<int, int> loc) {
 			break;
 		}
 		if (dol[y][i] == dol_color) {
-			garo++;
+			check_width++;
 		}
 		else {
 			break;
 		}
 	}
 
-	for (int i = x + 1; i <= X_LINE; i++) {
+	for (int i = x + 1; i <= WIDTH_LINE; i++) {
 		if (count++ > 5) {
 			break;
 		}
 		if (dol[y][i] == dol_color) {
-			garo++;
+			check_width++;
 		}
 		else {
 			break;
 		}
 	}
 
-	if (garo >= 5) {
+	if (check_width >= 5) {
 		return dol_color;
 	}
 
@@ -358,26 +503,26 @@ int checkGameOver(pair<int, int> loc) {
 			break;
 		}
 		if (dol[i][x] == dol_color) {
-			sero++;
+			check_height++;
 		}
 		else {
 			break;
 		}
 	}
 
-	for (int i = y + 1; i <= Y_LINE; i++) {
+	for (int i = y + 1; i <= HEIGHT_LINE; i++) {
 		if (count++ > 5) {
 			break;
 		}
 		if (dol[i][x] == dol_color) {
-			sero++;
+			check_height++;
 		}
 		else {
 			break;
 		}
 	}
 
-	if (sero >= 5) {
+	if (check_height >= 5) {
 		return dol_color;
 	}
 
@@ -390,7 +535,7 @@ int checkGameOver(pair<int, int> loc) {
 			break;
 		}
 		if (dol[i][temp_x--] == dol_color) {
-			tentofive++;
+			check_downward_diagonal++;
 		}
 		else {
 			break;
@@ -399,19 +544,19 @@ int checkGameOver(pair<int, int> loc) {
 
 	temp_x = x + 1;
 
-	for (int i = y + 1; i <= Y_LINE; i++) {
-		if (temp_x > X_LINE || count++ > 5) {
+	for (int i = y + 1; i <= HEIGHT_LINE; i++) {
+		if (temp_x > WIDTH_LINE || count++ > 5) {
 			break;
 		}
 		if (dol[i][temp_x++] == dol_color) {
-			tentofive++;
+			check_downward_diagonal++;
 		}
 		else {
 			break;
 		}
 	}
 
-	if (tentofive >= 5) {
+	if (check_downward_diagonal >= 5) {
 		return dol_color;
 	}
 
@@ -419,30 +564,30 @@ int checkGameOver(pair<int, int> loc) {
 
 	temp_x = x + 1;
 	for (int i = y - 1; i >= 0; i--) {
-		if (temp_x > X_LINE || count++ > 5) {
+		if (temp_x > WIDTH_LINE || count++ > 5) {
 			break;
 		}
 		if (dol[i][temp_x++] == dol_color) {
-			seventotwo++;
+			check_upward_diagonal++;
 		}
 		else {
 			break;
 		}
 	}
 	temp_x = x - 1;
-	for (int i = y + 1; i <= Y_LINE; i++) {
+	for (int i = y + 1; i <= HEIGHT_LINE; i++) {
 		if (temp_x < 0 || count++ > 5) {
 			break;
 		}
 		if (dol[i][temp_x--] == dol_color) {
-			seventotwo++;
+			check_upward_diagonal++;
 		}
 		else {
 			break;
 		}
 	}
 
-	if (seventotwo >= 5) {
+	if (check_upward_diagonal >= 5) {
 		return dol_color;
 	}
 
@@ -452,6 +597,35 @@ int checkGameOver(pair<int, int> loc) {
 }
 */
 
+bool cmp(PII a, PII b) {
+	if (a.first == b.first) {
+		return a.second > b.second;
+	} 
+	return a.first > b.first;
+}
+
+void WinDolChange(HWND hwnd, int dol_color) {
+	PAINTSTRUCT ps;
+	sort(completed_dol_loc.begin(), completed_dol_loc.end(), cmp);
+	for (PII loc : completed_dol_loc) {
+		cout << loc.first << loc.second << endl;
+		if (dol_color == BLACK) {
+			dol[loc.first][loc.second] = BLACK_WIN;
+		}
+		else if (dol_color == WHITE) {
+			dol[loc.first][loc.second] = WHITE_WIN;
+		}
+	}
+
+
+
+	InvalidateRect(hwnd, NULL, TRUE);
+	drawMap(hwnd);
+
+	EndPaint(hwnd, &ps);
+	return;
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
 	
 	if (umsg == WM_PAINT) {
@@ -459,64 +633,64 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
 	}
 	else if (umsg == WM_LBUTTONDOWN) {
 		
-		auto loc = buttonDown(hwnd, (int)LOWORD(lparam), (int)HIWORD(lparam));
+		PII loc = buttonDown(hwnd, (int)HIWORD(lparam), (int)LOWORD(lparam));
+		if (loc.first == -1 && loc.second == -1) {
+			return DefWindowProc(hwnd, umsg, wparam, lparam);
+		}
+		// 1 vs 1
 		if (game_mode == 1) {
 			unsigned int value = checkLineValue(loc);
-			if (order == 2 && value == 10) {
+
+			// 순서는 돌을 놓는 순간 전환되기 때문에 order가 반대일 경우로 계산
+			if (order == WHITE && value >= WIN_SCORE) {
+				//cout << completed_dol_loc.size() << endl;
+				WinDolChange(hwnd, BLACK);
 				MessageBox(hwnd, L"흑돌승리", L"Black Win!", MB_OK);
 				memset(dol, 0, sizeof(dol));
-				order = 1;
+				order = BLACK;
 				InvalidateRect(hwnd, NULL, TRUE);
 			}
-			else if (order == 1 && value == 10) {
+			else if (order == BLACK && value >= WIN_SCORE) {
+				WinDolChange(hwnd, WHITE);
 				MessageBox(hwnd, L"백돌승리", L"White Win!", MB_OK);
 				memset(dol, 0, sizeof(dol));
-				order = 1;
+				order = BLACK;
 				InvalidateRect(hwnd, NULL, TRUE);
 			}
 		}
+		// vs computer
 		else {
 			unsigned int value = checkLineValue(loc);
-			if (value == 10) {
+			// 컴퓨터가 돌을 놓는 것을 좀 더 자연스럽게 검은 돌 두고 화면 갱신
+			InvalidateRect(hwnd, NULL, TRUE);
+			drawMap(hwnd);
+			if (value >= WIN_SCORE) {
+				WinDolChange(hwnd, BLACK);
 				MessageBox(hwnd, L"흑돌승리", L"Black Win!", MB_OK);
 				memset(dol, 0, sizeof(dol));
-				order = 1;
+				order = BLACK;
 				InvalidateRect(hwnd, NULL, TRUE);
 			}
 			else {
-				if (order == 1) {
-					order++;
-				}
-				else {
-					order--;
-				}
-				pair<int, int> com = whiteAI();
-				if (order == 1) {
-					order++;
-				}
-				else {
-					order--;
-				}
-
-				computer_dol.first = com.first;
-				computer_dol.second = com.second;
-
-				dol[computer_dol.first][computer_dol.second] = 2;
-				cout << computer_dol.first << computer_dol.second << endl;
-				value = checkLineValue(computer_dol);
-				cout << value << endl;
-				if (value == 10) {
+				PII com = whiteAI();
+				
+				dol[com.first][com.second] = 2;
+				order = WHITE;
+				value = checkLineValue(com);
+				order = BLACK;
+				// 컴퓨터가 돌을 놓는 것을 좀 더 자연스럽게 흰 돌 두고 1초뒤 화면 갱신
+				Sleep(1000);
+				InvalidateRect(hwnd, NULL, TRUE);
+				drawMap(hwnd);
+				if (value >= WIN_SCORE) {
+					WinDolChange(hwnd, WHITE);
 					MessageBox(hwnd, L"백돌승리", L"Black Win!", MB_OK);
 					memset(dol, 0, sizeof(dol));
-					order = 1;
+					order = BLACK;
 					InvalidateRect(hwnd, NULL, TRUE);
 				}
 			}
-
-
 		}
-
-
 	}
 	else if (umsg == WM_DESTROY) {
 		PostQuitMessage(0);
@@ -545,7 +719,7 @@ int WINAPI main(HINSTANCE hIns, HINSTANCE hPrevIns, LPSTR lpCmdLine, int nCmdSho
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindow(class_name, class_name, WS_OVERLAPPEDWINDOW, 0, 0, 
-		2 * WHITE_SPACE + Y_LINE * INTERVAL, 2 * WHITE_SPACE + X_LINE * INTERVAL, 
+		2 * WHITE_SPACE + HEIGHT_LINE * INTERVAL, 2 * WHITE_SPACE + WIDTH_LINE * INTERVAL, 
 		NULL, NULL, hIns, NULL);
 
 	ShowWindow(hwnd, SW_SHOW);
